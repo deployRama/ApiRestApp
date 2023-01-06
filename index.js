65b308d609f8dcdd1ee3756cbf8efd9ffd60053c
Ramiro <deployrama@gmail.com>
2023-01-05 21:50:07 -0300

    Simple cors solucionado

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.APP_PORT || 3001;
 
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://totheblock.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('We are running...');
